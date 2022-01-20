6fb635e2f9c6adee5485703250e34556c108494d
barmao <bryanbarmao@gmail.com>
2022-01-20 22:31:59 +0300

    added boilerplate code

--- server.js
+++ server.js
@@ -24,7 +24,54 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//api endpoint to convert normal date to unix or convert unix time to normal date
+app.get("/api/:date", function (req, res) {
+  var date = req.params.date;
 
+  //Check if date is normal date or unix date
+  //Check if normal date is valid, if not say invalid
+
+  if(!isNaN(date)){
+    //If is valid date, convert to unix
+    console.log('Date Valid');
+
+    const convertedDate = new Date(date);
+
+    res.json({
+      unix:date,
+      utc:convertedDate.toUTCString
+    });
+
+  } 
+  
+  
+  // else if(){
+  //   //If is unix value, convert to normal date
+  //   console.log('Date Valid Is Not Valid');
+  //   res.json({
+  //     unix:'ppppp',
+  //     utc:'kkkkkk'
+  //   });
+  // } 
+  
+  else {
+    console.log('Date Valid Is Not Valid');
+    res.json({
+      unix:'ppppp',
+      utc:'kkkkkk'
+    });
+  }
+
+  //res.json({greeting: 'hello API ' + date});
+});
+
+var checkIfDateIsValid = function isDate(dateStr) {
+  return !isNaN(new Date(dateStr).getDate());
+}
+
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
