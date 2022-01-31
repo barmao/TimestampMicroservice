e7525435f1c2215f1c75e3f561ade31d223e142a
barmao <bryanbarmao@gmail.com>
2022-01-31 20:46:11 +0300

    updated date formatting logic

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+var moment = require('moment');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -27,51 +28,25 @@ app.get("/api/hello", function (req, res) {
 //api endpoint to convert normal date to unix or convert unix time to normal date
 app.get("/api/:date", function (req, res) {
   var date = req.params.date;
-
   //Check if date is normal date or unix date
   //Check if normal date is valid, if not say invalid
 
   if(!isNaN(date)){
-    //If is valid date, convert to unix
-    console.log('Date Valid');
-
-    const convertedDate = new Date(date);
-
+    var dateString = moment.unix(1451001600000).utc();
     res.json({
       unix:date,
-      utc:convertedDate.toUTCString
+      utc:dateString
     });
-
-  } 
-  
-  
-  // else if(){
-  //   //If is unix value, convert to normal date
-  //   console.log('Date Valid Is Not Valid');
-  //   res.json({
-  //     unix:'ppppp',
-  //     utc:'kkkkkk'
-  //   });
-  // } 
-  
-  else {
-    console.log('Date Valid Is Not Valid');
+  } else if(moment(date).isValid()){
+    var tmsx = moment(date).format("X");
     res.json({
-      unix:'ppppp',
-      utc:'kkkkkk'
+      unix:tmsx,
+      utc:date
     });
+  } else {
+    res.json('Date is not valide');
   }
-
-  //res.json({greeting: 'hello API ' + date});
 });
-
-var checkIfDateIsValid = function isDate(dateStr) {
-  return !isNaN(new Date(dateStr).getDate());
-}
-
-function isValidDate(d) {
-  return d instanceof Date && !isNaN(d);
-}
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
